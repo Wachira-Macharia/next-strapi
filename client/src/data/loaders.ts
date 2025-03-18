17b6267c63cd763fdb00d542c2eff95a1744feaa
Wachira-Macharia <eugenecomboni@gmail.com>
2025-03-18 17:19:29 +0300

    Building the experience page

--- client/src/data/loaders.ts
+++ client/src/data/loaders.ts
@@ -2,6 +2,7 @@ import qs from "qs";
 import { fetchAPI } from "@/utils/fetch-api";
 import { getStrapiURL } from "@/utils/get-strapi-url";
 
+const BASE_URL = getStrapiURL();
 const homePageQuery = qs.stringify({
   populate: {
     blocks: {
@@ -36,10 +37,53 @@ const homePageQuery = qs.stringify({
 
 export async function getHomePage() {
   const path = "/api/home-page";
-  const BASE_URL = getStrapiURL();
   const url = new URL(path, BASE_URL);
-
   url.search = homePageQuery;
 
-  return fetchAPI(url.href, { method: "GET" });
+  return await fetchAPI(url.href, { method: "GET" });
+}
+
+const pageBySlugQuery = (slug: string) =>
+  qs.stringify({
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              logo: {
+                populate: {
+                  image: {
+                    fields: ["url", "alternativeText"],
+                  },
+                },
+              },
+              cta: true,
+            },
+          },
+          "blocks.info-block": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              cta: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+export async function getPageBySlug(slug: string) {
+  const path = "/api/pages";
+  const url = new URL(path, BASE_URL);
+  url.search = pageBySlugQuery(slug);
+  return await fetchAPI(url.href, { method: "GET" });
 }
